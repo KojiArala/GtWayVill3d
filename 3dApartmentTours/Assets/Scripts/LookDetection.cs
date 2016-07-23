b217e0506e1d774e41939128cba83f3c0460c04b
KojiArala <kojiarala@hotmail.com>
2016-07-23 08:33:57 -0700

    Scripts

---
+++ 3dApartmentTours/Assets/Scripts/LookDetection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookDetection : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)){
+			if(hit.collider.name == "Cube")
+				{
+				}
+			if(hit.collider.name == "Door")
+			{
+				hit.collider.SendMessageUpwards ("animateDoor");
+			}
+			if (hit.collider.name == "AnimationTestCube")
+			{
+				hit.collider.SendMessageUpwards ("animateCube");
+			}
+
+		}
+	}
+}
---
+++ 3dApartmentTours/Assets/Scripts/NewAnimatedDoor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewAnimatedDoor : MonoBehaviour 
+{
+	
+	Animator anim;
+	public int openHash = Animator.StringToHash ("Door Open");
+	public int closeHash = Animator.StringToHash ("DoorClose");
+	public bool doorOpen = false;
+		
+	
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	
+	void animateDoor()
+	{
+		if (Input.GetKeyDown (KeyCode.F) && (anim.GetCurrentAnimatorStateInfo(0).IsName("Door Idle"))) 
+		{
+			print ("input working");
+			anim.SetTrigger (openHash);
+		}
+		if (Input.GetKeyDown (KeyCode.F) && (anim.GetCurrentAnimatorStateInfo(0).IsName("DoorIdleOpen")))
+		{
+			anim.SetTrigger (closeHash);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
