afa5defa2f91d008ca335c17a924d7b8f3ade38c
KojiArala <kojiarala@hotmail.com>
2016-07-23 08:35:40 -0700

    MiscLegacy

---
+++ 3dApartmentTours/3d Tour Assets/3dApartmentTours/Assets/Scripts/ClickedOnTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickedOnTest : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)){
+			if(hit.collider.name == "Cube")
+				{
+				hit.collider.SendMessageUpwards ("testSay");
+				}
+			if(hit.collider.name == "Door")
+			{
+				hit.collider.SendMessageUpwards ("openDoor");
+			}
+			if (hit.collider.name == "AnimationTestCube")
+			{
+				hit.collider.SendMessageUpwards ("animateCube");
+			}
+
+		}
+	}
+}
