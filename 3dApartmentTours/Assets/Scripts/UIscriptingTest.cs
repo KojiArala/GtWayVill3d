47b07ffa51c711ee8c3a22ffdd0be5dc7a0699a0
KojiArala <kojiarala@hotmail.com>
2016-08-25 17:59:45 -0700

    8-25-2016

--- 3dApartmentTours/Assets/Scripts/UIscriptingTest.cs
+++ 3dApartmentTours/Assets/Scripts/UIscriptingTest.cs
@@ -5,41 +5,100 @@ using System.Collections;
 public class UIscriptingTest : MonoBehaviour {
 
 	public Text accentText;
+	public Image wButton;
+	public Image aButton;
+	public Image sButton;
+	public Image dButton;
+	public Image cButton;
+	public Sprite wupSprite;
+	public Sprite wdownSprite;
+	public Sprite aupSprite;
+	public Sprite adownSprite;
+	public Sprite supSprite;
+	public Sprite sdownSprite;
+	public Sprite dupSprite;
+	public Sprite ddownSprite;
+	public Sprite cupSprite;
+	public Sprite cdownSprite;
 	public float fadeSpeed = 5f;
-	public bool lookingAtWall;
-	public GameObject accentCanvas;
+	public bool inLivingRoom;
+	public GameObject roomCanvas;
 
-	void Start ()
+	void Awake ()
 	{
-		accentText = accentCanvas.GetComponentInChildren<Text> ();
+		accentText = roomCanvas.GetComponentInChildren<Text> ();
+		wButton = roomCanvas.GetComponentInChildren<Image> ();
 		accentText.color = Color.clear;
+		wButton.sprite = wupSprite;
+		aButton.sprite = aupSprite;
+		sButton.sprite = supSprite;
+		dButton.sprite = dupSprite;
+
 	}
 
 
-	void ColorChange()
+	void Update ()
 	{
-		if (lookingAtWall) 
+		ShowAccentInfo ();
+		UIhighlights ();
+	}
+
+
+	void ShowAccentInfo()
+	{
+		if (inLivingRoom) 
 		{
 			accentText.color = Color.Lerp (accentText.color, Color.white, fadeSpeed = Time.deltaTime);
 		}
-		if (!lookingAtWall) 
+		if (!inLivingRoom) 
 		{
 			accentText.color = Color.Lerp (accentText.color, Color.clear, fadeSpeed = Time.deltaTime);
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		ColorChange ();	
-		if (Input.GetKeyDown (KeyCode.T)) 
-		    {
-			lookingAtWall = true;
-		}
-		if (Input.GetKeyDown (KeyCode.Y)) 
-		    {
-			lookingAtWall = false;
-		}
 
+	void UIhighlights()
+	{
+		if (Input.GetKeyDown (KeyCode.W)) 
+		{
+			wButton.sprite = wdownSprite;
+		}
+		if (Input.GetKeyUp (KeyCode.W)) 
+		{
+			wButton.sprite = wupSprite;
+		}
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			aButton.sprite = adownSprite;
+		}
+		if (Input.GetKeyUp (KeyCode.A)) 
+		{
+			aButton.sprite = aupSprite;
+		}
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			sButton.sprite = sdownSprite;
+		}
+		if (Input.GetKeyUp (KeyCode.S)) 
+		{
+			sButton.sprite = supSprite;
+		}
+		if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			dButton.sprite = ddownSprite;
+		}
+		if (Input.GetKeyUp (KeyCode.D)) 
+		{
+			dButton.sprite = dupSprite;
+		}
 	}
+
+	public void MessageTest1()
+	{
+		inLivingRoom == true;
+	}
+	public void MessageTest2()
+	{
+		inLivingRoom == false;
+	}
+
 }
