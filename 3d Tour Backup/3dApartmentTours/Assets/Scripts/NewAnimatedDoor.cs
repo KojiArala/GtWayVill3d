a1b48e608afebbb260b688a1c5b4a4331bebb2d6
KojiArala <kojiarala@hotmail.com>
2017-01-16 15:43:44 -0800

    Major Commit (Main files uploaded)

---
+++ 3d Tour Backup/3dApartmentTours/Assets/Scripts/NewAnimatedDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewAnimatedDoor : MonoBehaviour 
+{
+	
+	Animator anim;
+	public int openHash = Animator.StringToHash ("Door Open");
+	public int closeHash = Animator.StringToHash ("DoorClose");
+	public bool doorOpen = false;
+		
+	
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	
+	void animateDoor()
+	{
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Door Idle")) 
+		{
+			anim.SetTrigger (openHash);
+		}
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("DoorIdleOpen"))
+		{
+			anim.SetTrigger (closeHash);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
---
+++ 3d Tour Backup/3dApartmentTours/Assets/Scripts/UICollider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICollider : MonoBehaviour {
+
+	public string uiCommand;
+	public string uiCommand2;
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			GameObject.Find("UIcontroller").GetComponent<UIscriptingTest>().MessageTest1();
+		}
+		
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			GameObject.Find("UIcontroller").GetComponent<UIscriptingTest>().MessageTest2();
+		}
+		
+	}
+}
